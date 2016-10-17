c9ddd3248f916ca2b1b376155db4f862b6523d49
Nader Dabit <dabit3@gmail.com>
2016-10-17 17:00:28 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	    "extends": "airbnb",
+	    "plugins": [
+		            "react",
+			            "jsx-a11y",
+				            "import"
+					        
+	    ],
+	    "rules": {
+		          "no-console": "off",
+			        "arrow-body-style": "off"
+				    
+	    }
+
+};
